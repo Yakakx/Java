573e1ed17915b8e91f55d1ac660364fbcd4866f5
KAI-AN YANG <90344069+Yakakx@users.noreply.github.com>
2023-11-29 21:16:44 +0800

    9_10

--- Chapter9/src/e10/Test.java
+++ Chapter9/src/e10/Test.java
@@ -1,10 +1,24 @@
 package e10;
+import java.util.*;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a, b, and c: ");
+		QuadraticEquation qe = 
+				new QuadraticEquation(input.nextDouble(),input.nextDouble(),input.nextDouble());
+		if(qe.getDiscriminant()==0) {
+			System.out.print(qe.getRoot1());
+		}
+		else if(qe.getDiscriminant()>0) {
+			System.out.println(qe.getRoot1());
+			System.out.println(qe.getRoot2());
+		}
+		else {
+			System.out.println("The equation has no roots.");
+		}
 	}
 
 }
